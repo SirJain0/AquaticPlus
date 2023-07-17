b28bef584b8df04bb8b5ef21d23d3de2f60ba6bf
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-17 17:57:33 -0400

    Initial set up of environment

---
+++ src/main/java/com/aquaticplus/AquaticPlusClient.java
@@ -0,0 +1,10 @@
+package com.aquaticplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AquaticPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
