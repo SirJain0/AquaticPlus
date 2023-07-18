ce637f44827ba5f7bc35d2b4e773d3c5836e27bf
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-18 16:43:53 -0400

    Fix missing texture

--- src/main/java/com/sirjain/entities/renderers/JohnsonRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/JohnsonRenderer.java
@@ -9,7 +9,7 @@ import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
 public class JohnsonRenderer extends MobEntityRenderer<JohnsonEntity, JohnsonModel> {
-    public static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/johnson/johnson.png");
+    public static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entities/johnson/johnson.png");
 
     public JohnsonRenderer(EntityRendererFactory.Context context) {
         super(context, new JohnsonModel(context.getPart(AquaticPlusEntities.JOHNNSON_LAYER)), 0.5f);
