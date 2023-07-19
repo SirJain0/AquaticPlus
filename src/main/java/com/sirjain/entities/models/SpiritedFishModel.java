c20d210bf30c27b977dffc4fc85e27e967b11a13
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-19 12:21:17 -0400

    Add model
    Fix missing translations

--- src/main/java/com/sirjain/entities/models/SpiritedFishModel.java
+++ src/main/java/com/sirjain/entities/models/SpiritedFishModel.java
@@ -15,10 +15,14 @@ public class SpiritedFishModel extends EntityModel<SpiritedFishEntity> {
         root = part;
     }
 
-    // TODO: Fill with geo
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
+        ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create().uv(18, 0).cuboid(-1.5F, -3.0F, -0.75F, 3.0F, 3.0F, 4.0F, new Dilation(0.0F))
+                .uv(0, 0).cuboid(-2.0F, -4.0F, -2.0F, 4.0F, 5.0F, 10.0F, new Dilation(0.0F))
+                .uv(16, 15).cuboid(-1.5F, -2.5F, -6.0F, 3.0F, 3.0F, 4.0F, new Dilation(0.0F))
+                .uv(0, 14).cuboid(0.5F, -7.0F, -1.0F, 0.0F, 3.0F, 8.0F, new Dilation(0.0F))
+                .uv(0, 7).cuboid(0.5F, -5.0F, 8.0F, 0.0F, 7.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 21.0F, -2.0F));
 
         return TexturedModelData.of(modelData, 32, 32);
     }
--- src/main/java/com/sirjain/entities/renderers/SpiritedFishRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/SpiritedFishRenderer.java
@@ -4,12 +4,14 @@ import com.sirjain.AquaticPlus;
 import com.sirjain.entities.entity.SpiritedFishEntity;
 import com.sirjain.entities.models.SpiritedFishModel;
 import com.sirjain.registries.AquaticPlusEntities;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 public class SpiritedFishRenderer extends MobEntityRenderer<SpiritedFishEntity, SpiritedFishModel> {
-    public static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entities/johnson/johnson.png");
+    public static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entities/spirited_fish/spirited_fish.png");
 
     public SpiritedFishRenderer(EntityRendererFactory.Context context) {
         super(context, new SpiritedFishModel(context.getPart(AquaticPlusEntities.SPIRITED_FISH_LAYER)), 0.5f);
@@ -20,5 +22,9 @@ public class SpiritedFishRenderer extends MobEntityRenderer<SpiritedFishEntity, 
         return TEXTURE;
     }
 
-    // TODO: When model implemented, make outer layer translucent
+    @Nullable
+    @Override
+    protected RenderLayer getRenderLayer(SpiritedFishEntity entity, boolean showBody, boolean translucent, boolean showOutline) {
+        return super.getRenderLayer(entity, showBody, true, showOutline);
+    }
 }
