196cc87b5c4ca03c69c28ec665a53cc02ec14af3
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-19 12:52:23 -0400

    Change texture depending on health

--- src/main/java/com/sirjain/entities/renderers/SpiritedFishRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/SpiritedFishRenderer.java
@@ -11,7 +11,9 @@ import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
 public class SpiritedFishRenderer extends MobEntityRenderer<SpiritedFishEntity, SpiritedFishModel> {
-    public static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entities/spirited_fish/spirited_fish.png");
+    public static final Identifier TEXTURE_FULL = new Identifier(AquaticPlus.MOD_ID, "textures/entities/spirited_fish/spirited_fish_full.png");
+    public static final Identifier TEXTURE_4 = new Identifier(AquaticPlus.MOD_ID, "textures/entities/spirited_fish/spirited_fish_4.png");
+    public static final Identifier TEXTURE_2 = new Identifier(AquaticPlus.MOD_ID, "textures/entities/spirited_fish/spirited_fish_2.png");
 
     public SpiritedFishRenderer(EntityRendererFactory.Context context) {
         super(context, new SpiritedFishModel(context.getPart(AquaticPlusEntities.SPIRITED_FISH_LAYER)), 0.5f);
@@ -19,7 +21,12 @@ public class SpiritedFishRenderer extends MobEntityRenderer<SpiritedFishEntity, 
 
     @Override
     public Identifier getTexture(SpiritedFishEntity entity) {
-        return TEXTURE;
+        return switch((int) entity.getHealth()) {
+            case 6, 5 -> TEXTURE_FULL;
+            case 4, 3 -> TEXTURE_4;
+            case 2, 1, 0 -> TEXTURE_2;
+            default -> TEXTURE_FULL;
+        };
     }
 
     @Nullable
