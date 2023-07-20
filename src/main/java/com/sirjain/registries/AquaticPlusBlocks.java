c2a89922035841999715102a06d38748fedd2c9a
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-20 13:03:39 -0400

    Add another utility registration method for blocks

--- src/main/java/com/sirjain/registries/AquaticPlusBlocks.java
+++ src/main/java/com/sirjain/registries/AquaticPlusBlocks.java
@@ -1,6 +1,7 @@
 package com.sirjain.registries;
 
 import com.sirjain.AquaticPlus;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,11 +12,19 @@ public class AquaticPlusBlocks {
         // Register blocks here
     }
 
-    public static Block registerBlock(String identifier, Block block) {
+    public static Block registerAdvancedBlock(String identifier, Block block) {
         return Registry.register(
                 Registries.BLOCK,
                 new Identifier(AquaticPlus.MOD_ID, identifier),
                 block
+        );
+    }
+
+    public static Block registerBasicBlock(String identifier, FabricBlockSettings settings) {
+        return Registry.register(
+                Registries.BLOCK,
+                new Identifier(AquaticPlus.MOD_ID, identifier),
+                new Block(settings)
         );
     }
 }
