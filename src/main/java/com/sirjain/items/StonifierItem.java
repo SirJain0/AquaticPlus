50da7406084fc14b4cc430839ce0883ccb6fabc4
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-20 12:57:20 -0400

    Add Stonifier and Destonifier

---
+++ src/main/java/com/sirjain/items/StonifierItem.java
@@ -0,0 +1,50 @@
+package com.sirjain.items;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StonifierItem extends Item {
+    public StonifierItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        PlayerEntity user = context.getPlayer();
+        BlockPos pos = context.getBlockPos();
+        List<BlockState> disallowedBlocks = Arrays.asList(
+                Blocks.END_PORTAL.getDefaultState(),
+                Blocks.BEDROCK.getDefaultState(),
+                Blocks.OBSIDIAN.getDefaultState(),
+                Blocks.CHEST.getDefaultState(),
+                Blocks.SHULKER_BOX.getDefaultState(),
+                Blocks.DRAGON_EGG.getDefaultState(),
+                Blocks.BARRIER.getDefaultState(),
+                Blocks.NETHER_PORTAL.getDefaultState(),
+                Blocks.END_PORTAL.getDefaultState(),
+                Blocks.END_GATEWAY.getDefaultState()
+        );
+
+        if (user == null) return ActionResult.PASS;
+
+        if (!disallowedBlocks.contains(world.getBlockState(pos)))
+            world.setBlockState(user.isSneaking() ? pos.up() : pos, Blocks.STONE.getDefaultState());
+
+        if (!user.getAbilities().creativeMode) {
+            user.getStackInHand(user.getActiveHand()).damage(1, user, (p) -> p.sendToolBreakStatus(user.getActiveHand()));
+            user.getItemCooldownManager().set(this, 40);
+        }
+
+        return super.useOnBlock(context);
+    }
+}
