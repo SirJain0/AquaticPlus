4af8f47a0beac38402d4622203facadfbc849337
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-21 09:22:16 -0400

    Update DetoxifyingAcidItem.java

--- src/main/java/com/sirjain/items/DetoxifyingAcidItem.java
+++ src/main/java/com/sirjain/items/DetoxifyingAcidItem.java
@@ -1,6 +1,6 @@
 package com.sirjain.items;
 
-import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -9,9 +9,6 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class DetoxifyingAcidItem extends Item {
     public DetoxifyingAcidItem(Settings settings) {
@@ -23,13 +20,14 @@ public class DetoxifyingAcidItem extends Item {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
         PlayerEntity user = context.getPlayer();
-        List<BlockState> copperStates = Arrays.asList(
-                Blocks.EXPOSED_COPPER.getDefaultState(),
-                Blocks.WEATHERED_COPPER.getDefaultState(),
-                Blocks.OXIDIZED_COPPER.getDefaultState()
-        );
 
-        if (copperStates.contains(world.getBlockState(pos)))
+        if (isState(world, pos, Blocks.OXIDIZED_COPPER))
+            world.setBlockState(pos, Blocks.WEATHERED_COPPER.getDefaultState());
+
+        else if (isState(world, pos, Blocks.WEATHERED_COPPER))
+            world.setBlockState(pos, Blocks.EXPOSED_COPPER.getDefaultState());
+
+        else if (isState(world, pos, Blocks.EXPOSED_COPPER))
             world.setBlockState(pos, Blocks.COPPER_BLOCK.getDefaultState());
 
         if (user != null && user.getAbilities().creativeMode) {
@@ -38,5 +36,9 @@ public class DetoxifyingAcidItem extends Item {
         }
 
         return super.useOnBlock(context);
+    }
+
+    public boolean isState(World world, BlockPos pos, Block block) {
+        return world.getBlockState(pos) == block.getDefaultState();
     }
 }
