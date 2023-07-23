6e001288382084ce05ea4bfdf5c08348ad50fff5
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-23 18:08:31 +0100

    Work on implementing dropping mechanism - buggy

--- src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
+++ src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
@@ -3,6 +3,7 @@ package com.sirjain.status_effects;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
 
 public class NumbingStatusEffect extends StatusEffect {
@@ -14,6 +15,12 @@ public class NumbingStatusEffect extends StatusEffect {
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         Vec3d velocityPriorEffect = entity.getVelocity();
         entity.setVelocity(velocityPriorEffect.x * 0.8f, velocityPriorEffect.y, velocityPriorEffect.z * 0.8f);
+
+        if (entity.handSwinging && entity.getWorld().random.nextInt(9) == 0) {
+            ItemStack stackInActiveHand = entity.getStackInHand(entity.getActiveHand());
+            entity.dropItem(stackInActiveHand.getItem());
+            stackInActiveHand.decrement(1);
+        }
     }
 
     @Override
