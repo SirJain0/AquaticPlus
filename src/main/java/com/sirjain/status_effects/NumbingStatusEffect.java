851f438bfc0e525e4f283d87db25b33c58399640
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-24 17:36:22 +0100

    Re-implement random

--- src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
+++ src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
@@ -34,8 +34,9 @@ public class NumbingStatusEffect extends StatusEffect {
     }
 
     public boolean canDropItemInHand(LivingEntity entity, World world, int amplifier) {
-        return entity.handSwingProgress > 0.83 && !world.isClient;
-//                world.random.nextInt(10 - (amplifier / 2 + 1)) == 0;
+        return entity.handSwingProgress > 0.83 &&
+                !world.isClient &&
+                world.random.nextInt(7 - (amplifier / 2 + 1)) == 0;
     }
 
     @Override
