4c5b3a2019f800b717ee9b3dceac5cee57002050
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-24 18:00:58 +0100

    Fix entity copying

--- src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
+++ src/main/java/com/sirjain/status_effects/NumbingStatusEffect.java
@@ -1,16 +1,12 @@
 package com.sirjain.status_effects;
 
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public class NumbingStatusEffect extends StatusEffect {
     public NumbingStatusEffect(StatusEffectCategory category, int color) {
@@ -21,13 +17,17 @@ public class NumbingStatusEffect extends StatusEffect {
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         Vec3d velocityPriorEffect = entity.getVelocity();
         World world = entity.getWorld();
-        entity.setVelocity(velocityPriorEffect.x * 0.85f, velocityPriorEffect.y, velocityPriorEffect.z * 0.85f);
+        entity.setVelocity(
+                velocityPriorEffect.x * 0.85f,
+                velocityPriorEffect.y,
+                velocityPriorEffect.z * 0.85f
+        );
 
         if (canDropItemInHand(entity, world, amplifier)) {
             ItemStack stackInActiveHand = entity.getStackInHand(entity.getActiveHand());
 
             if (entity instanceof PlayerEntity) ((PlayerEntity) entity).dropItem(stackInActiveHand.copyWithCount(1), false, true);
-            else entity.dropItem(entity.getStackInHand(entity.getActiveHand()).getItem().asItem());
+            else entity.dropStack(stackInActiveHand.copyWithCount(1));
 
             stackInActiveHand.decrement(1);
         }
