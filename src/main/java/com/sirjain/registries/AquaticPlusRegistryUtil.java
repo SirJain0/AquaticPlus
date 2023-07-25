ade244719e0b147e10775255f87d372fc2921b15
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-25 13:47:17 +0100

    Add Numbing status effect

--- src/main/java/com/sirjain/registries/AquaticPlusRegistryUtil.java
+++ src/main/java/com/sirjain/registries/AquaticPlusRegistryUtil.java
@@ -1,12 +1,15 @@
 package com.sirjain.registries;
 
 import com.sirjain.items.AquaticPlusItemGroup;
+import com.sirjain.status_effects.AquaticPlusPotions;
 
 public class AquaticPlusRegistryUtil {
     public static void initContent() {
         AquaticPlusEntities.registerAttributes();
         AquaticPlusEntities.registerEntityRender();
+        AquaticPlusStatusEffects.registerModEffects();
         AquaticPlusItems.registerItems();
+        AquaticPlusPotions.registerPotionRecipes();
         AquaticPlusBlocks.registerBlocks();
         AquaticPlusItems.registerBlockItems();
         AquaticPlusItemGroup.registerItemGroup();
---
+++ src/main/java/com/sirjain/status_effects/AquaticPlusPotions.java
@@ -0,0 +1,33 @@
+package com.sirjain.status_effects;
+
+import com.sirjain.AquaticPlus;
+import com.sirjain.mixins.BrewingRecipeRegistryMixin;
+import com.sirjain.registries.AquaticPlusItems;
+import com.sirjain.registries.AquaticPlusStatusEffects;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class AquaticPlusPotions {
+    public static Potion NUMBING_POTION;
+
+    public static void registerPotions() {
+        // TODO: Add these potions to custom item group, not default group
+        NUMBING_POTION = registerPotion("numbing_potion", new Potion(new StatusEffectInstance(AquaticPlusStatusEffects.NUMBING, 20*10, 0)));
+    }
+
+    private static Potion registerPotion(String name, Potion potion) {
+        return Registry.register(
+                Registries.POTION,
+                new Identifier(AquaticPlus.MOD_ID, name),
+                potion
+        );
+    }
+
+    public static void registerPotionRecipes() {
+        // TODO: Add numbing potion recipe with fibula and stonefish venom
+    }
+}
