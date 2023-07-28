ada9ed64388a380ddf04dca6c048c91cefdee50c
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-28 15:55:02 +0100

    Make kelp eel flee players

--- src/main/java/com/sirjain/entities/entity/KelpEelEntity.java
+++ src/main/java/com/sirjain/entities/entity/KelpEelEntity.java
@@ -3,8 +3,10 @@ package com.sirjain.entities.entity;
 import com.sirjain.entities.entity.template.AbstractEelEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.EscapeDangerGoal;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 public class KelpEelEntity extends AbstractEelEntity {
@@ -16,6 +18,7 @@ public class KelpEelEntity extends AbstractEelEntity {
         protected void initGoals() {
                 super.initGoals();
                 this.goalSelector.add(0, new EscapeDangerGoal(this, 1.5f));
+                this.goalSelector.add(1, new FleeEntityGoal<>(this, PlayerEntity.class, 9, 1.0, 1.5));
         }
 
         public static DefaultAttributeContainer.Builder createKelpEelAttributes() {
