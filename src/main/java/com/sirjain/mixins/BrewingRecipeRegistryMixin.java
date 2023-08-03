d73db8b79af5f7a53763d434587532f73a5940e1
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-03 13:35:19 +0100

    Add long/strong variants to potions

--- src/main/java/com/sirjain/mixins/BrewingRecipeRegistryMixin.java
+++ src/main/java/com/sirjain/mixins/BrewingRecipeRegistryMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(BrewingRecipeRegistry.class)
 public interface BrewingRecipeRegistryMixin {
         @Invoker("registerPotionRecipe")
-        static void invokeRegisterPotionRecipe(Potion input, Item item, Potion output) {
+        static void addRecipe(Potion input, Item item, Potion output) {
                 throw new AssertionError();
         }
 }
