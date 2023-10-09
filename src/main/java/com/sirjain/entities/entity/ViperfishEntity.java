c1735de6c5062f582e4f461a0f0e5cf9907bbb67
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-09 18:53:02 -0400

    Make viperfish emissive texture only appear when it's dark

--- src/main/java/com/sirjain/entities/entity/ViperfishEntity.java
+++ src/main/java/com/sirjain/entities/entity/ViperfishEntity.java
@@ -1,26 +1,31 @@
 package com.sirjain.entities.entity;
 
 import com.sirjain.entities.entity.template.NoBucketFishEntity;
-import com.sirjain.registries.AquaticPlusStatusEffects;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.EscapeDangerGoal;
-import net.minecraft.entity.ai.goal.FleeEntityGoal;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.ElderGuardianEntity;
 import net.minecraft.entity.mob.GuardianEntity;
 import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ViperfishEntity extends NoBucketFishEntity {
+	public static final TrackedData<Boolean> SHOULD_ILLUMINATE = DataTracker.registerData(ViperfishEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
 	public ViperfishEntity(EntityType<? extends FishEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -31,6 +36,34 @@ public class ViperfishEntity extends NoBucketFishEntity {
 		this.goalSelector.add(isLowOnHealth() ? 4 : 0, new MeleeAttackGoal(this, 1.4, true));
 		this.goalSelector.add(isLowOnHealth() ? 0 : 5, new EscapeDangerGoal(this, 1.4f));
 		this.targetSelector.add(0, new RevengeGoal(this, ElderGuardianEntity.class, GuardianEntity.class));
+	}
+
+	@Override
+	protected void initDataTracker() {
+		super.initDataTracker();
+		this.dataTracker.startTracking(SHOULD_ILLUMINATE, false);
+	}
+
+	@Override
+	public void writeCustomDataToNbt(NbtCompound nbt) {
+		super.writeCustomDataToNbt(nbt);
+		nbt.putBoolean("should_illuminate", this.dataTracker.get(SHOULD_ILLUMINATE));
+	}
+
+	@Override
+	public void readCustomDataFromNbt(NbtCompound nbt) {
+		super.readCustomDataFromNbt(nbt);
+		this.dataTracker.set(SHOULD_ILLUMINATE, nbt.getBoolean("should_illuminate"));
+	}
+
+	public void setShouldIlluminate(boolean shouldIlluminate) {
+		this.dataTracker.set(SHOULD_ILLUMINATE, shouldIlluminate);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		this.setShouldIlluminate(this.getWorld().getBrightness(BlockPos.ofFloored(this.getPos())) <= 0.5);
 	}
 
 	// TODO: Change to generic sound flop
--- src/main/java/com/sirjain/entities/renderers/ViperfishRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/ViperfishRenderer.java
@@ -2,7 +2,7 @@ package com.sirjain.entities.renderers;
 
 import com.sirjain.AquaticPlus;
 import com.sirjain.entities.entity.ViperfishEntity;
-import com.sirjain.entities.features.APEmissiveTextureFeature;
+import com.sirjain.entities.features.ViperfishEmissiveTextureFeature;
 import com.sirjain.entities.models.ViperfishModel;
 import com.sirjain.registries.AquaticPlusEntities;
 import net.minecraft.client.render.entity.EntityRendererFactory;
@@ -11,11 +11,10 @@ import net.minecraft.util.Identifier;
 
 public class ViperfishRenderer extends MobEntityRenderer<ViperfishEntity, ViperfishModel> {
 	private static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/viperfish/viperfish.png");
-	private static final Identifier EMISSIVE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/viperfish/viperfish_e.png");
 
 	public ViperfishRenderer(EntityRendererFactory.Context context) {
 		super(context, new ViperfishModel(context.getPart(AquaticPlusEntities.VIPERFISH_LAYER)), 0.5f);
-		this.addFeature(new APEmissiveTextureFeature<>(this, EMISSIVE));
+		this.addFeature(new ViperfishEmissiveTextureFeature<>(this));
 	}
 
 	@Override
