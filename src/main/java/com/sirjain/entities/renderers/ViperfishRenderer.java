d4a49918e4e5438d625af19d70ff33a2c3bfa65c
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-09 14:22:58 -0400

    Cleanup

--- src/main/java/com/sirjain/entities/renderers/ViperfishRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/ViperfishRenderer.java
@@ -3,12 +3,10 @@ package com.sirjain.entities.renderers;
 import com.sirjain.AquaticPlus;
 import com.sirjain.entities.entity.ViperfishEntity;
 import com.sirjain.entities.features.APEmissiveTextureFeature;
-import com.sirjain.entities.models.SpiritedFishModel;
 import com.sirjain.entities.models.ViperfishModel;
 import com.sirjain.registries.AquaticPlusEntities;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
 public class ViperfishRenderer extends MobEntityRenderer<ViperfishEntity, ViperfishModel> {
