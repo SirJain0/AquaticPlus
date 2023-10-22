88d3fbdeb85591e3a026dff12af91fe2e3280c94
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-21 23:21:08 -0400

    Add random textures to rays

--- src/main/java/com/sirjain/entities/entity/MantaRayEntity.java
+++ src/main/java/com/sirjain/entities/entity/MantaRayEntity.java
@@ -1,17 +1,60 @@
 package com.sirjain.entities.entity;
 
 import com.sirjain.entities.entity.template.NoBucketSchoolingFishEntity;
+import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.SchoolingFishEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.function.ValueLists;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-// Refer to project doc for plans
+import java.util.function.IntFunction;
 
 public class MantaRayEntity extends NoBucketSchoolingFishEntity {
+	private static final TrackedData<Integer> MANTA_RAY_TYPE = DataTracker.registerData(MantaRayEntity.class, TrackedDataHandlerRegistry.INTEGER);
+
 	public MantaRayEntity(EntityType<? extends SchoolingFishEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@Nullable
+	@Override
+	public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+		this.initVariant();
+		return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+	}
+
+	protected void initDataTracker() {
+		super.initDataTracker();
+		this.dataTracker.startTracking(MANTA_RAY_TYPE, MantaRayType.DARK.id);
+	}
+
+	protected void initVariant() {
+		int textureID = this.random.nextInt(3);
+
+		if (textureID == 0) this.setVariant(MantaRayType.DARK);
+		else if (textureID == 1) this.setVariant(MantaRayType.DARK_SPOTTED);
+		else if (textureID == 2) this.setVariant(MantaRayType.BLUE);
+	}
+
+	public void writeCustomDataToNbt(NbtCompound nbt) {
+		super.writeCustomDataToNbt(nbt);
+		nbt.putInt("MantaRayType", this.getVariant().id);
+	}
+
+	public void readCustomDataFromNbt(NbtCompound nbt) {
+		super.readCustomDataFromNbt(nbt);
+		this.setVariant(MantaRayEntity.MantaRayType.byId(nbt.getInt("MantaRayType")));
 	}
 
 	public static DefaultAttributeContainer.Builder createMantaRayAttributes() {
@@ -19,5 +62,40 @@ public class MantaRayEntity extends NoBucketSchoolingFishEntity {
 			.createFishAttributes()
 			.add(EntityAttributes.GENERIC_MAX_HEALTH, 22)
 			.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 3.6f);
+	}
+
+	public void setVariant(MantaRayType mantaRayType) {
+		this.dataTracker.set(MANTA_RAY_TYPE, mantaRayType.id);
+	}
+
+	public MantaRayEntity.MantaRayType getVariant() {
+		return MantaRayEntity.MantaRayType.byId(this.dataTracker.get(MANTA_RAY_TYPE));
+	}
+
+	public enum MantaRayType implements StringIdentifiable {
+		DARK(0, "dark"),
+		DARK_SPOTTED(1, "dark_spotted"),
+		BLUE(2, "blue");
+
+		private static final IntFunction<MantaRayEntity.MantaRayType> BY_ID = ValueLists.createIdToValueFunction(MantaRayEntity.MantaRayType::getId, values(), DARK);
+		final int id;
+		private final String name;
+
+		MantaRayType(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public String asString() {
+			return this.name;
+		}
+
+		public int getId() {
+			return this.id;
+		}
+
+		public static MantaRayEntity.MantaRayType byId(int id) {
+			return BY_ID.apply(id);
+		}
 	}
 }
--- src/main/java/com/sirjain/entities/renderers/MantaRayRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/MantaRayRenderer.java
@@ -9,7 +9,9 @@ import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.util.Identifier;
 
 public class MantaRayRenderer extends LivingEntityRenderer<MantaRayEntity, MantaRayModel> {
-	private static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/manta_ray/manta_ray_1.png");
+	private static final Identifier DARK_TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/manta_ray/manta_ray_dark.png");
+	private static final Identifier DARK_SPOTTED_TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/manta_ray/manta_ray_dark_spotted.png");
+	private static final Identifier BLUE_TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/manta_ray/manta_ray_blue.png");
 
 	public MantaRayRenderer(EntityRendererFactory.Context context) {
 		super(context, new MantaRayModel(context.getPart(AquaticPlusEntities.MANTA_RAY_LAYER)), 0.75f);
@@ -17,6 +19,10 @@ public class MantaRayRenderer extends LivingEntityRenderer<MantaRayEntity, Manta
 
 	@Override
 	public Identifier getTexture(MantaRayEntity entity) {
-		return TEXTURE;
+		return switch(entity.getVariant()) {
+			case DARK -> DARK_TEXTURE;
+			case DARK_SPOTTED -> DARK_SPOTTED_TEXTURE;
+			case BLUE -> BLUE_TEXTURE;
+		};
 	}
 }
