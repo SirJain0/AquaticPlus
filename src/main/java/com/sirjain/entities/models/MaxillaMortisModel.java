4cfd1aaabc51d6e43cc50b2267019aea35be88cf
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-03-13 09:38:26 +0530

    Retexture Maxilla Mortis

--- src/main/java/com/sirjain/entities/models/MaxillaMortisModel.java
+++ src/main/java/com/sirjain/entities/models/MaxillaMortisModel.java
@@ -36,17 +36,17 @@ public class MaxillaMortisModel extends EntityModel<MaxillaMortisEntity> {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
 
-		ModelPartData main = modelPartData.addChild("main", ModelPartBuilder.create().uv(0, 0).cuboid(-3.0F, -4.1965F, -7.8536F, 6.0F, 8.0F, 16.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 18.1965F, -9.0037F));
-		ModelPartData upper_fin_r1 = main.addChild("upper_fin_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -7.0F, -2.0F, 2.0F, 10.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -4.1965F, -1.8536F, -0.7854F, 0.0F, 0.0F));
-		ModelPartData side_fin_r_r1 = main.addChild("side_fin_r_r1", ModelPartBuilder.create().uv(28, 0).mirrored().cuboid(-7.0F, 0.0F, -6.0F, 7.0F, 1.0F, 12.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(-3.0F, 1.8035F, 0.1464F, 0.0F, 0.0F, -0.1745F));
-		ModelPartData side_fin_l_r1 = main.addChild("side_fin_l_r1", ModelPartBuilder.create().uv(28, 0).cuboid(0.0F, 0.0F, -6.0F, 7.0F, 1.0F, 12.0F, new Dilation(0.0F)), ModelTransform.of(3.0F, 1.8035F, 0.1464F, 0.0F, 0.0F, 0.1745F));
-		ModelPartData head = main.addChild("head", ModelPartBuilder.create().uv(43, 29).cuboid(-2.5F, -3.0F, -5.975F, 5.0F, 6.0F, 7.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.3035F, -7.8536F));
-		ModelPartData jaw_upper = head.addChild("jaw_upper", ModelPartBuilder.create().uv(0, 44).cuboid(-2.0F, -1.7382F, -4.3625F, 4.0F, 3.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -0.5F, -6.5F, -1.0472F, 0.0F, 0.0F));
-		ModelPartData jaw_lower = head.addChild("jaw_lower", ModelPartBuilder.create(), ModelTransform.of(0.0F, 0.5F, -6.5F, 0.8727F, 0.0F, 0.0F));
-		ModelPartData cube_r1 = jaw_lower.addChild("cube_r1", ModelPartBuilder.create().uv(0, 44).cuboid(-2.0F, -1.7107F, -4.3232F, 4.0F, 3.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 3.1416F));
-		ModelPartData tail_1 = main.addChild("tail_1", ModelPartBuilder.create().uv(0, 24).cuboid(-2.0F, -3.0F, 0.0F, 4.0F, 6.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0535F, 8.1464F));
-		ModelPartData tail_2 = tail_1.addChild("tail_2", ModelPartBuilder.create().uv(25, 36).cuboid(-1.0F, -1.5F, 0.0F, 2.0F, 4.0F, 11.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -0.5F, 14.0F));
-		ModelPartData tail3 = tail_2.addChild("tail_3", ModelPartBuilder.create().uv(22, 10).cuboid(-1.5151F, -6.0F, -0.1659F, 0.0F, 12.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(1.5151F, 0.5F, 5.1659F));
+		ModelPartData main = modelPartData.addChild("main", ModelPartBuilder.create().uv(0, 0).cuboid(-4.1F, -5.0555F, -10.2096F, 8.0F, 10.0F, 21.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 18.1965F, -9.0037F));
+		ModelPartData upper_fin_r1 = main.addChild("upper_fin_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.6F, -9.1F, -2.6F, 3.0F, 13.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -5.4555F, -2.4096F, -0.7854F, 0.0F, 0.0F));
+		ModelPartData side_fin_r_r1 = main.addChild("side_fin_r_r1", ModelPartBuilder.create().uv(37, 0).mirrored().cuboid(-9.0F, 0.3F, -7.8F, 9.0F, 1.0F, 16.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(-3.9F, 2.3445F, 0.1904F, 0.0F, 0.0F, -0.1745F));
+		ModelPartData side_fin_l_r1 = main.addChild("side_fin_l_r1", ModelPartBuilder.create().uv(37, 0).cuboid(0.1F, 0.3F, -7.8F, 9.0F, 1.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(3.9F, 2.3445F, 0.1904F, 0.0F, 0.0F, 0.1745F));
+		ModelPartData head = main.addChild("head", ModelPartBuilder.create().uv(55, 38).cuboid(-3.75F, -4.1F, -7.7675F, 7.0F, 8.0F, 9.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.3945F, -10.2096F, 0.0F, -0.2182F, 0.0F));
+		ModelPartData jaw_upper = head.addChild("jaw_upper", ModelPartBuilder.create().uv(0, 57).cuboid(-2.4F, -2.3596F, -5.6713F, 5.0F, 4.0F, 7.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -0.65F, -8.45F, -1.0472F, 0.0F, 0.0F));
+		ModelPartData jaw_lower = head.addChild("jaw_lower", ModelPartBuilder.create(), ModelTransform.of(0.0F, 0.65F, -8.45F, 0.8727F, 0.0F, 0.0F));
+		ModelPartData cube_r1 = jaw_lower.addChild("cube_r1", ModelPartBuilder.create().uv(0, 57).cuboid(-2.4F, -2.3239F, -5.6202F, 5.0F, 4.0F, 7.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 3.1416F));
+		ModelPartData tail_1 = main.addChild("tail_1", ModelPartBuilder.create().uv(0, 31).cuboid(-2.4F, -4.1F, 0.0F, 5.0F, 8.0F, 18.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0695F, 10.5904F, 0.0F, 0.1745F, 0.0F));
+		ModelPartData tail_2 = tail_1.addChild("tail_2", ModelPartBuilder.create().uv(32, 47).cuboid(-1.7F, -1.75F, 0.0F, 3.0F, 5.0F, 14.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -0.65F, 18.2F, 0.0F, 0.3927F, 0.0F));
+		ModelPartData tail3 = tail_2.addChild("tail_3", ModelPartBuilder.create().uv(28, 13).cuboid(-0.1991F, -8.2F, -3.1458F, 0.0F, 16.0F, 18.0F, new Dilation(0.0F)), ModelTransform.of(-0.0304F, 0.65F, 10.7157F, 0.0F, 0.3491F, 0.0F));
 
 		return TexturedModelData.of(modelData, 128, 128);
 	}
--- src/main/java/com/sirjain/entities/renderers/MaxillaMortisRenderer.java
+++ src/main/java/com/sirjain/entities/renderers/MaxillaMortisRenderer.java
@@ -3,6 +3,7 @@ package com.sirjain.entities.renderers;
 import com.sirjain.AquaticPlus;
 import com.sirjain.entities.entity.MaxillaMortisEntity;
 import com.sirjain.entities.features.APEmissiveTextureFeature;
+import com.sirjain.entities.features.MaxillaMortisEmissiveTextureFeature;
 import com.sirjain.entities.models.MaxillaMortisModel;
 import com.sirjain.registries.AquaticPlusEntities;
 import net.minecraft.client.render.entity.EntityRendererFactory;
@@ -12,14 +13,12 @@ import net.minecraft.util.Identifier;
 import static com.sirjain.entities.entity.MaxillaMortisEntity.CAN_RECOLOR;
 
 public class MaxillaMortisRenderer extends MobEntityRenderer<MaxillaMortisEntity, MaxillaMortisModel> {
-	// TODO: Retexture
 	private static final Identifier TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/maxilla_mortis/maxilla_mortis.png");
 	private static final Identifier PURPLE_TEXTURE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/maxilla_mortis/maxilla_mortis_purple.png");
-	private static final Identifier EMISSIVE = new Identifier(AquaticPlus.MOD_ID, "textures/entity/maxilla_mortis/maxilla_mortis_e.png");
 
 	public MaxillaMortisRenderer(EntityRendererFactory.Context context) {
 		super(context, new MaxillaMortisModel(context.getPart(AquaticPlusEntities.MAXILLA_MORTIS_LAYER)), 0.5f);
-		this.addFeature(new APEmissiveTextureFeature<>(this, EMISSIVE));
+		this.addFeature(new MaxillaMortisEmissiveTextureFeature<>(this));
 	}
 
 	@Override
