2eff89243c1161f94d8e0f397904ffc608c5a759
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-03-16 16:57:26 +0530

    Add Lanternfish entity
    Add Ghostly Gel bbmodel

--- src/main/java/com/sirjain/registries/AquaticPlusItems.java
+++ src/main/java/com/sirjain/registries/AquaticPlusItems.java
@@ -56,6 +56,7 @@ public class AquaticPlusItems {
 	public static Item MANTA_RAY_SPAWN_EGG;
 	public static Item DUMBO_BLOB_SPAWN_EGG;
 	public static Item PINK_SUNFISH_SPAWN_EGG;
+	public static Item LANTERNFISH_SPAWN_EGG;
 
 	public static Item SPIRITED_FISH_BUCKET;
 	public static Item PARROTFISH_BUCKET;
@@ -113,6 +114,7 @@ public class AquaticPlusItems {
 		MANTA_RAY_SPAWN_EGG = registerSpawnEggItem("manta_ray_spawn_egg", AquaticPlusEntities.MANTA_RAY_ENTITY, 0x282a34, 0x999daa);
 		DUMBO_BLOB_SPAWN_EGG = registerSpawnEggItem("dumbo_blob_spawn_egg", AquaticPlusEntities.DUMBO_BLOB_ENTITY, 0x3981d2, 0xc36afa);
 		PINK_SUNFISH_SPAWN_EGG = registerSpawnEggItem("pink_sunfish_spawn_egg", AquaticPlusEntities.PINK_SUNFISH_ENTITY, 0xcb8fbc, 0x68759d);
+		LANTERNFISH_SPAWN_EGG = registerSpawnEggItem("lanternfish_spawn_egg", AquaticPlusEntities.LANTERNFISH_ENTITY, 0x33333b, 0x34d7e8);
 	}
 
 	public static void registerBucketItems() {
