b2f08b9251bd84da7d50a37bf486eb50cb49e62d
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-03-19 10:11:32 -0400

    Redo lanternfish

--- src/main/java/com/sirjain/entities/models/LanternfishModel.java
+++ src/main/java/com/sirjain/entities/models/LanternfishModel.java
@@ -35,15 +35,15 @@ public class LanternfishModel extends EntityModel<LanternfishEntity> {
 		ModelPartData cube_r1 = body.addChild("cube_r1", ModelPartBuilder.create().uv(12, 0).mirrored().cuboid(-7.0F, 0.0F, -2.0F, 7.0F, 0.0F, 6.0F, new Dilation(0.0F)).mirrored(false), ModelTransform.of(-2.0F, 1.7765F, 2.875F, 0.0F, 0.0F, -0.3491F));
 		ModelPartData cube_r2 = body.addChild("cube_r2", ModelPartBuilder.create().uv(12, 0).cuboid(0.0F, 0.0F, -2.0F, 7.0F, 0.0F, 6.0F, new Dilation(0.0F)), ModelTransform.of(2.0F, 1.7765F, 2.875F, 0.0F, 0.0F, 0.3491F));
 		ModelPartData body_2 = body.addChild("body_2", ModelPartBuilder.create().uv(0, 22).cuboid(-2.0F, -0.75F, 0.0F, 4.0F, 5.0F, 7.0F, new Dilation(0.0F))
-			.uv(0, 28).cuboid(0.0F, -5.75F, 1.0F, 0.0F, 6.0F, 6.0F, new Dilation(0.0F))
-			.uv(32, 7).cuboid(0.0F, 3.25F, 2.0F, 0.0F, 5.0F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -2.4735F, 6.875F));
-		ModelPartData body_3 = body_2.addChild("body_3", ModelPartBuilder.create().uv(14, 27).cuboid(-1.0F, -2.0F, 0.0F, 2.0F, 4.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 2.0F, 7.0F));
+			.uv(22, 28).cuboid(0.0F, 3.25F, 2.0F, 0.0F, 5.0F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -2.4735F, 6.875F));
+		ModelPartData cube_r3 = body_2.addChild("cube_r3", ModelPartBuilder.create().uv(22, 21).cuboid(0.0F, -3.0F, -3.0F, 0.0F, 6.0F, 6.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -1.75F, 4.0F, -0.3491F, 0.0F, 0.0F));
+		ModelPartData body_3 = body_2.addChild("body_3", ModelPartBuilder.create().uv(24, 0).cuboid(-1.0F, -2.0F, 0.0F, 2.0F, 4.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 2.0F, 7.0F));
 		ModelPartData tail = body_3.addChild("tail", ModelPartBuilder.create().uv(0, 0).cuboid(0.0F, -5.0F, -4.0F, 0.0F, 10.0F, 12.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 6.0F));
-		ModelPartData head = lanternfish.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-3.0F, -2.0F, -6.0F, 6.0F, 4.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -7.5F, -13.25F));
-		ModelPartData lower_jaw = head.addChild("lower_jaw", ModelPartBuilder.create().uv(26, 27).cuboid(-3.0F, 0.75F, -6.25F, 6.0F, 1.0F, 6.0F, new Dilation(0.3F)), ModelTransform.pivot(0.0F, 1.5F, 0.075F));
+		ModelPartData head = lanternfish.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-3.0F, -1.5F, -6.0F, 6.0F, 5.0F, 6.0F, new Dilation(0.05F)), ModelTransform.pivot(0.0F, -7.5F, -13.75F));
 
 		return TexturedModelData.of(modelData, 64, 64);
 	}
+
 
 	@Override
 	public void setAngles(LanternfishEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
