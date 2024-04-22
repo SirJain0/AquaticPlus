7e1e8e937060c4ebec96382117180215b4e92dc3
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-04-21 21:41:15 -0400

    Make frost version of jellyfish ridable

--- src/main/java/com/sirjain/entities/entity/JellyfishEntity.java
+++ src/main/java/com/sirjain/entities/entity/JellyfishEntity.java
@@ -1,9 +1,7 @@
 package com.sirjain.entities.entity;
 
 import com.sirjain.entities.entity.template.NoBucketSchoolingFishEntity;
-import net.minecraft.entity.EntityData;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.data.DataTracker;
@@ -11,9 +9,16 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.FishEntity;
 import net.minecraft.entity.passive.SchoolingFishEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.function.ValueLists;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
@@ -24,12 +29,12 @@ import java.util.function.IntFunction;
 /*
 TODO:
 - Add random scales
-- Make it ridable but not controllable
 - Make it hurt you when you touch it
 - Drops jelly and ghostly membrane
 - Flee when it is below a certain health
+- Animations
  */
-public class JellyfishEntity extends NoBucketSchoolingFishEntity {
+public class JellyfishEntity extends NoBucketSchoolingFishEntity implements Mount {
 	private static final TrackedData<Integer> JELLYFISH_TYPE = DataTracker.registerData(JellyfishEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
 	public JellyfishEntity(EntityType<? extends SchoolingFishEntity> entityType, World world) {
@@ -53,12 +58,67 @@ public class JellyfishEntity extends NoBucketSchoolingFishEntity {
 	public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
 		this.initVariant();
 
-		if (this.getVariant().id == 2) {
+		if (this.isFrostVariant()) {
 			this.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(15);
 			this.heal(this.getMaxHealth());
 		}
 
 		return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+	}
+
+	public boolean isFrostVariant() {
+		return this.getVariant().id == 2;
+	}
+
+	@Override
+	protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+		if (this.isFrostVariant() && hand == Hand.MAIN_HAND) {
+			this.setRiding(player);
+			return ActionResult.SUCCESS;
+		}
+
+		return super.interactMob(player, hand);
+	}
+
+	private void setRiding(PlayerEntity player) {
+		player.setYaw(this.getYaw());
+		player.setPitch(this.getPitch());
+		player.startRiding(this);
+	}
+
+	@Override
+	public double getMountedHeightOffset() {
+		return 1.1f;
+	}
+
+	@Override
+	public Vec3d updatePassengerForDismount(LivingEntity passenger) {
+		Direction direction = this.getMovementDirection();
+
+		if (direction.getAxis() == Direction.Axis.Y) {
+			return super.updatePassengerForDismount(passenger);
+		}
+
+		int[][] is = Dismounting.getDismountOffsets(direction);
+		BlockPos blockPos = this.getBlockPos();
+		BlockPos.Mutable mutable = new BlockPos.Mutable();
+		for (EntityPose entityPose : passenger.getPoses()) {
+			Box box = passenger.getBoundingBox(entityPose);
+
+			for (int[] js : is) {
+				mutable.set(blockPos.getX() + js[0], blockPos.getY(), blockPos.getZ() + js[1]);
+				double d = this.getWorld().getDismountHeight(mutable);
+
+				if (!Dismounting.canDismountInBlock(d)) continue;
+				Vec3d vec3d = Vec3d.ofCenter(mutable, d);
+				if (!Dismounting.canPlaceEntityAt(this.getWorld(), passenger, box.offset(vec3d))) continue;
+				passenger.setPose(entityPose);
+
+				return vec3d;
+			}
+		}
+
+		return super.updatePassengerForDismount(passenger);
 	}
 
 	protected void initVariant() {
