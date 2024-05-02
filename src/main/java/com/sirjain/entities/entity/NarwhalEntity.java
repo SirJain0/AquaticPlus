2b33460a3d86fde4ac6d70f827265dc3507f16e4
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-05-01 22:27:44 -0400

    Update NarwhalEntity.java

--- src/main/java/com/sirjain/entities/entity/NarwhalEntity.java
+++ src/main/java/com/sirjain/entities/entity/NarwhalEntity.java
@@ -38,7 +38,8 @@ import org.jetbrains.annotations.Nullable;
 
 /*
 TODO:
-- Look into being tameable
+- Why is it staying still?
+- Fix speed
  */
 public class NarwhalEntity extends AnimalEntity implements Saddleable, Mount {
 	private static final TrackedData<Boolean> SADDLED = DataTracker.registerData(NarwhalEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
@@ -50,7 +51,7 @@ public class NarwhalEntity extends AnimalEntity implements Saddleable, Mount {
 	public NarwhalEntity(EntityType<? extends AnimalEntity> entityType, World world) {
 		super(entityType, world);
 		this.saddledComponent = new SaddledComponent(this.dataTracker, BOOST_TIME, SADDLED);
-		this.moveControl = new AquaticMoveControl(this, 85, 10, 0.05F, 0.1F, true);
+		this.moveControl = new AquaticMoveControl(this, 85, 10, 1, 0.04f, true);
 		this.lookControl = new YawAdjustingLookControl(this, 10);
 	}
 
@@ -61,15 +62,15 @@ public class NarwhalEntity extends AnimalEntity implements Saddleable, Mount {
 
 	@Override
 	protected void initGoals() {
-		this.goalSelector.add(1, new TemptGoal(this, 1, Ingredient.ofItems(AquaticPlusItems.HALIBUT), false));
-		this.goalSelector.add(0, new MoveIntoWaterGoal(this));
-		this.goalSelector.add(4, new SwimAroundGoal(this, 1.0, 10));
-		this.goalSelector.add(4, new LookAroundGoal(this));
-		this.goalSelector.add(5, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+		this.goalSelector.add(1, new SwimAroundGoal(this, 1.0, 10));
+		this.goalSelector.add(2, new LookAroundGoal(this));
+		this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
 		this.goalSelector.add(8, new ChaseBoatGoal(this));
+
 		this.goalSelector.add(9, new FleeEntityGoal<>(this, GuardianEntity.class, 8.0F, 1.0, 1.0));
 		this.goalSelector.add(2, new AnimalMateGoal(this, 1.3D));
 		this.goalSelector.add(4, new FollowParentGoal(this, 1.1));
+		this.goalSelector.add(1, new TemptGoal(this, 1, Ingredient.ofItems(AquaticPlusItems.HALIBUT), false));
 	}
 
 	@Nullable
@@ -191,11 +192,6 @@ public class NarwhalEntity extends AnimalEntity implements Saddleable, Mount {
 
 				super.travel(new Vec3d(sidewaysSpeed, movementInput.y, forwardSpeed));
 			}
-		} else {
-			if (this.isLogicalSideForUpdatingMovement())
-				this.move(MovementType.SELF, this.getRotationVector().multiply(0.15f));
-
-			super.travel(movementInput.multiply(2f));
 		}
 
 		if (!this.isSubmergedInWater() && this.isLogicalSideForUpdatingMovement()) {
