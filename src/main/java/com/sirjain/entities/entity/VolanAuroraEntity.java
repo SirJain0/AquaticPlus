79d123db2534d05e1fbc55d2ec0e6da102b4f6ce
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-05-13 21:50:45 -0400

    Ambient particles + healing

--- src/main/java/com/sirjain/entities/entity/VolanAuroraEntity.java
+++ src/main/java/com/sirjain/entities/entity/VolanAuroraEntity.java
@@ -6,13 +6,13 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.FishEntity;
 import net.minecraft.entity.passive.SchoolingFishEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.World;
 
 /*
 TODO:
 - Try to make custom spawn egg
 - Make it attack guardians via projectile, and change texture if angry
-- Emit particles
 - Think of an item that baits it - maybe a spectre of some sort?
 - Emissive texture for eyes - change based on variant
  */
@@ -32,5 +32,33 @@ public class VolanAuroraEntity extends NoBucketSchoolingFishEntity {
 	@Override
 	public float getSpeedAmplifier() {
 		return 0.11f;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+
+		if (this.getWorld().isClient) {
+			World world = this.getWorld();
+			double xPos = this.getX() + 0.8f;
+			double xPos2 = this.getX() - 0.8f;
+
+			summonRandomMagicParticle(world, xPos);
+			summonRandomMagicParticle(world, xPos2);
+		}
+
+		if (this.age % 70 == 0 && this.getHealth() < this.getMaxHealth()) {
+			this.heal(2);
+		}
+	}
+
+	public void summonRandomMagicParticle(World world, double x) {
+		if (this.random.nextInt(7) == 0) {
+			world.addParticle(
+				ParticleTypes.END_ROD,
+				x, this.getRandomBodyY(), this.getZ() + 0.5f,
+				-0.01f, 0.05f, 0
+			);
+		}
 	}
 }
