f5eb3754aea2e566ea221c5b5c33b5bf8377c744
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-05-18 22:46:17 -0400

    Start implementation and add assets

---
+++ src/main/java/com/aquaticplus/entities/entity/PikeEntity.java
@@ -0,0 +1,96 @@
+package com.aquaticplus.entities.entity;
+
+import com.aquaticplus.entities.entity.template.NoBucketSchoolingFishEntity;
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.SchoolingFishEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.function.ValueLists;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.IntFunction;
+
+public class PikeEntity extends NoBucketSchoolingFishEntity {
+	private static final TrackedData<Integer> PIKE_TYPE = DataTracker.registerData(PikeEntity.class, TrackedDataHandlerRegistry.INTEGER);
+
+	public PikeEntity(EntityType<? extends SchoolingFishEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Nullable
+	@Override
+	public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+		this.initVariant();
+		return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
+	}
+
+	protected void initVariant() {
+		int textureID = this.random.nextInt(3); // change to 45
+
+		if (textureID == 0) this.setVariant(PikeType.RARE);
+		else this.setVariant(PikeType.BROWN);
+	}
+
+	@Override
+	protected void initDataTracker() {
+		super.initDataTracker();
+		this.dataTracker.startTracking(PIKE_TYPE, PikeType.BROWN.id);
+	}
+
+	@Override
+	public void writeCustomDataToNbt(NbtCompound nbt) {
+		super.writeCustomDataToNbt(nbt);
+		nbt.putInt("pike_type", this.getVariant().id);
+	}
+
+	@Override
+	public void readCustomDataFromNbt(NbtCompound nbt) {
+		super.readCustomDataFromNbt(nbt);
+		this.setVariant(PikeType.byId(nbt.getInt("pike_type")));
+	}
+
+	public void setVariant(PikeType pikeType) {
+		this.dataTracker.set(PIKE_TYPE, pikeType.id);
+	}
+
+	public PikeType getVariant() {
+		return PikeType.byId(this.dataTracker.get(PIKE_TYPE));
+	}
+
+	public enum PikeType implements StringIdentifiable {
+		BROWN(0, "pike_brown"),
+		RARE(1, "pike_purple");
+
+		private static final IntFunction<PikeType> BY_ID = ValueLists.createIdToValueFunction(
+			PikeType::getId, values(), BROWN
+		);
+
+		final int id;
+		private final String name;
+
+		PikeType(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public String asString() {
+			return this.name;
+		}
+
+		public int getId() {
+			return this.id;
+		}
+
+		public static PikeType byId(int id) {
+			return BY_ID.apply(id);
+		}
+	}
+}
