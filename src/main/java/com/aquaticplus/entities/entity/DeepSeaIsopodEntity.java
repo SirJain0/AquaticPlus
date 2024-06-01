5528544e51785c2a2ff101c87120accf34938762
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-05-31 20:55:33 -0400

    Update DeepSeaIsopodEntity.java

--- src/main/java/com/aquaticplus/entities/entity/DeepSeaIsopodEntity.java
+++ src/main/java/com/aquaticplus/entities/entity/DeepSeaIsopodEntity.java
@@ -1,6 +1,7 @@
 package com.aquaticplus.entities.entity;
 
-import net.minecraft.entity.EntityType;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.EscapeDangerGoal;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
@@ -11,8 +12,13 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,7 +28,7 @@ TODO:
 - mountable feature
 - Bellyrubs??
  */
-public class DeepSeaIsopodEntity extends WaterCreatureEntity {
+public class DeepSeaIsopodEntity extends WaterCreatureEntity implements Mount {
 	public static final TrackedData<Boolean> IS_FROSTPOD = DataTracker.registerData(DeepSeaIsopodEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
 
 	public DeepSeaIsopodEntity(EntityType<? extends WaterCreatureEntity> entityType, World world) {
@@ -67,6 +73,75 @@ public class DeepSeaIsopodEntity extends WaterCreatureEntity {
 
 	public void setFrostpodState(boolean val) {
 		this.dataTracker.set(IS_FROSTPOD, val);
+	}
+
+	@Override
+	public void travel(Vec3d movementInput) {
+		if (this.hasPassengers() && getControllingPassenger() instanceof PlayerEntity && this.isSubmergedInWater()) {
+			LivingEntity rider = this.getControllingPassenger();
+
+			this.setYaw(rider.getYaw());
+			this.prevYaw = this.getYaw();
+			this.setPitch(rider.getPitch() * 0.5F);
+			this.setRotation(this.getYaw(), this.getPitch());
+			this.bodyYaw = this.getYaw();
+			this.headYaw = this.bodyYaw;
+			float sidewaysSpeed = rider.sidewaysSpeed * 0.5F;
+			float forwardSpeed = rider.forwardSpeed;
+
+			if (forwardSpeed <= 0.0F)
+				forwardSpeed *= 0.25F;
+
+			if (this.isLogicalSideForUpdatingMovement()) {
+				this.setMovementSpeed((float) this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED));
+				this.move(MovementType.SELF, this.getRotationVector().multiply(
+					MinecraftClient.getInstance().options.sprintKey.isPressed()
+						? forwardSpeed / 1.5f
+						: forwardSpeed / 2.5f
+				));
+
+				super.travel(new Vec3d(sidewaysSpeed, movementInput.y, forwardSpeed));
+			}
+		} else {
+			if (this.isLogicalSideForUpdatingMovement())
+				this.move(MovementType.SELF, this.getRotationVector().multiply(0.11f));
+
+			super.travel(movementInput.multiply(2f));
+		}
+
+		if (!this.isSubmergedInWater() && this.isLogicalSideForUpdatingMovement()) {
+			this.setVelocity(this.getVelocity().x, -0.7f, this.getVelocity().z);
+		}
+	}
+
+	@Override
+	public Vec3d updatePassengerForDismount(LivingEntity passenger) {
+		Direction direction = this.getMovementDirection();
+
+		if (direction.getAxis() == Direction.Axis.Y) {
+			return super.updatePassengerForDismount(passenger);
+		}
+
+		int[][] is = Dismounting.getDismountOffsets(direction);
+		BlockPos blockPos = this.getBlockPos();
+		BlockPos.Mutable mutable = new BlockPos.Mutable();
+		for (EntityPose entityPose : passenger.getPoses()) {
+			Box box = passenger.getBoundingBox(entityPose);
+
+			for (int[] js : is) {
+				mutable.set(blockPos.getX() + js[0], blockPos.getY(), blockPos.getZ() + js[1]);
+				double d = this.getWorld().getDismountHeight(mutable);
+
+				if (!Dismounting.canDismountInBlock(d)) continue;
+				Vec3d vec3d = Vec3d.ofCenter(mutable, d);
+				if (!Dismounting.canPlaceEntityAt(this.getWorld(), passenger, box.offset(vec3d))) continue;
+				passenger.setPose(entityPose);
+
+				return vec3d;
+			}
+		}
+
+		return super.updatePassengerForDismount(passenger);
 	}
 
 	public static DefaultAttributeContainer.Builder createIsopodAttributes() {
