aa3e3f2f31b9ec525b013584d3bd1587b52cdd2b
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-06-08 10:10:28 -0400

    Basic AI w/o fight

--- src/main/java/com/aquaticplus/entities/entity/boss/PhantomJellyfishEntity.java
+++ src/main/java/com/aquaticplus/entities/entity/boss/PhantomJellyfishEntity.java
@@ -1,14 +1,33 @@
 package com.aquaticplus.entities.entity.boss;
 
 import com.aquaticplus.entities.entity.template.APFishEntity;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.FishEntity;
 import net.minecraft.world.World;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableRangedAttack;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyPlayersSensor;
 
-public class PhantomJellyfishEntity extends APFishEntity {
+import java.util.List;
+
+public class PhantomJellyfishEntity extends APFishEntity implements SmartBrainOwner<PhantomJellyfishEntity> {
 	public PhantomJellyfishEntity(EntityType<? extends FishEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -36,5 +55,66 @@ public class PhantomJellyfishEntity extends APFishEntity {
 			.createFishAttributes()
 			.add(EntityAttributes.GENERIC_MAX_HEALTH, 110)
 			.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 10);
+	}
+
+	// Brain
+
+	// Replaces Vanilla brain
+	@Override
+	protected Brain.Profile<?> createBrainProfile() {
+		return new SmartBrainProvider<>(this);
+	}
+
+	// Tick the brain so it functions
+	@Override
+	protected void mobTick() {
+		super.mobTick();
+		this.tickBrain(this);
+	}
+
+	// Gives the entity sensors - things it is conscious of
+	@Override
+	public List<? extends ExtendedSensor<? extends PhantomJellyfishEntity>> getSensors() {
+		return ObjectArrayList.of(
+			new NearbyPlayersSensor<>(),            // Tracks nearby players
+			new NearbyLivingEntitySensor<>()        // Tracks nearby entities
+		);
+	}
+
+	// Usually run all the time
+	@Override
+	public BrainActivityGroup<PhantomJellyfishEntity> getCoreTasks() {
+		return BrainActivityGroup.coreTasks(
+			new MoveToWalkTarget<>()                // Walk towards the current walk target
+		);
+	}
+
+	// Tasks run when nothing else is happening
+	@Override
+	public BrainActivityGroup<PhantomJellyfishEntity> getIdleTasks() { // These are the tasks that run when the mob isn't doing anything else (usually)
+		return BrainActivityGroup.idleTasks(
+			// Try each one in order
+			new FirstApplicableBehaviour<PhantomJellyfishEntity>(
+				new TargetOrRetaliate<>(),      // Set the attack target and walk target based on nearby entities
+				new SetRandomLookTarget<>()     // Set a random look target
+			),
+
+			// Pick a random one
+			new OneRandomBehaviour<>(
+				new SetRandomWalkTarget<>(),    // Set a random walk target to a nearby position
+				new Idle<>().runFor(entity -> entity.getRandom().nextBetween(20, 40))
+			)
+		);
+	}
+
+	// Runs when the entity is fighting
+	@Override
+	public BrainActivityGroup<PhantomJellyfishEntity> getFightTasks() {
+		return BrainActivityGroup.fightTasks(
+			new InvalidateAttackTarget<>(),         // Cancel fighting if the target is no longer valid
+			new OneRandomBehaviour<>(
+				new AnimatableRangedAttack<>(10)
+			)
+		);
 	}
 }
