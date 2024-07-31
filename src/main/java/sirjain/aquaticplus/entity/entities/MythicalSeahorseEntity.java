28ddd3c4fb4cae888c4b0d030db26ec3c1a6f815
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-07-31 20:52:47 +0100

    Get attack w/ temporary projectile working
    Next step: Add spike items

--- src/main/java/sirjain/aquaticplus/entity/entities/MythicalSeahorseEntity.java
+++ src/main/java/sirjain/aquaticplus/entity/entities/MythicalSeahorseEntity.java
@@ -2,13 +2,19 @@ package sirjain.aquaticplus.entity.entities;
 
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.RangedAttackMob;
+import net.minecraft.entity.ai.goal.ProjectileAttackGoal;
+import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.FishEntity;
 import net.minecraft.entity.passive.SchoolingFishEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.function.ValueLists;
@@ -16,16 +22,37 @@ import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-import sirjain.aquaticplus.entity.entities.template.AbstractEelEntity;
+import sirjain.aquaticplus.entity.entities.projectile.AuroraEntity;
 import sirjain.aquaticplus.entity.entities.template.NoBucketSchoolingFishEntity;
 
 import java.util.function.IntFunction;
 
-public class MythicalSeahorseEntity extends NoBucketSchoolingFishEntity {
+public class MythicalSeahorseEntity extends NoBucketSchoolingFishEntity implements RangedAttackMob {
 	private static final TrackedData<Integer> MYTHICAL_SEAHORSE_TYPE = DataTracker.registerData(MythicalSeahorseEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
 	public MythicalSeahorseEntity(EntityType<? extends SchoolingFishEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@Override
+	protected void initGoals() {
+		super.initGoals();
+
+		this.goalSelector.add(0, new ProjectileAttackGoal(this, 1, 40, 15));
+		this.targetSelector.add(0, new RevengeGoal(this));
+	}
+
+	@Override
+	public void attack(LivingEntity target, float pullProgress) {
+		double xCoord = target.getX() - this.getX();
+		double zCoord = target.getZ() - this.getZ();
+		double velPath = Math.sqrt(xCoord * xCoord + zCoord * zCoord);
+
+		ThrownItemEntity projectile = new AuroraEntity(this.getWorld(), this);
+		double yCoord = target.getBodyY(0.3f) - projectile.getY();
+
+		projectile.setVelocity(xCoord, yCoord + velPath * (double) 0.2f, zCoord, 1, 0);
+		this.getWorld().spawnEntity(projectile);
 	}
 
 	// == VARIANTS ==
@@ -100,9 +127,10 @@ public class MythicalSeahorseEntity extends NoBucketSchoolingFishEntity {
 	}
 
 	public static DefaultAttributeContainer.Builder createMythicalSeahorseAttributes() {
-		return AbstractEelEntity
-			.createGenericEelAttributes()
+		return FishEntity
+			.createFishAttributes()
 			.add(EntityAttributes.GENERIC_FOLLOW_RANGE, 10)
+			.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2)
 			.add(EntityAttributes.GENERIC_MAX_HEALTH, 15);
 	}
 }
