bb45f96bd260a9f0f0cbe5c9fdd7e9a25fe4fdcd
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-08-08 08:45:37 +0100

    Finish tridents

--- src/main/java/sirjain/aquaticplus/entity/entities/projectile/AquaSeaTridentEntity.java
+++ src/main/java/sirjain/aquaticplus/entity/entities/projectile/AquaSeaTridentEntity.java
@@ -1,17 +1,58 @@
 package sirjain.aquaticplus.entity.entities.projectile;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import sirjain.aquaticplus.entity.AquaticPlusEntities;
 import sirjain.aquaticplus.entity.entities.template.SeaTridentEntity;
+import sirjain.aquaticplus.item.AquaticPlusItems;
+import sirjain.aquaticplus.status_effect.AquaticPlusStatusEffects;
+
+import java.util.List;
 
 public class AquaSeaTridentEntity extends SeaTridentEntity {
 	public AquaSeaTridentEntity(EntityType<? extends SeaTridentEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
+	@Override
+	protected void onEntityHit(EntityHitResult entityHitResult) {
+		Entity entityFromHit = entityHitResult.getEntity();
+		World world = entityFromHit.getWorld();
+
+		if (entityFromHit instanceof LivingEntity target) {
+			List<Entity> entities = world.getOtherEntities(target, target.getBoundingBox().expand(2), EntityPredicates.VALID_LIVING_ENTITY);
+
+			for (Entity entityAround : entities) {
+				if (entityAround == this.getOwner()) return;
+				((LivingEntity) entityAround).addStatusEffect(new StatusEffectInstance(AquaticPlusStatusEffects.LAND_DROWNING, 20*4, 1));
+			}
+		}
+
+		super.onEntityHit(entityHitResult);
+	}
+
 	public AquaSeaTridentEntity(World world, PlayerEntity player) {
 		super(AquaticPlusEntities.AQUA_SEA_TRIDENT_ENTITY, world, player);
+	}
+
+	@Override
+	protected ItemStack asItemStack() {
+		return AquaticPlusItems.AQUA_SEA_TRIDENT.getDefaultStack();
+	}
+
+	@Override
+	public ParticleEffect getParticle() {
+		return ParticleTypes.END_ROD;
 	}
 }
--- src/main/java/sirjain/aquaticplus/entity/entities/template/SeaTridentEntity.java
+++ src/main/java/sirjain/aquaticplus/entity/entities/template/SeaTridentEntity.java
@@ -1,11 +1,15 @@
 package sirjain.aquaticplus.entity.entities.template;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import sirjain.aquaticplus.item.AquaticPlusItems;
 
 public abstract class SeaTridentEntity extends PersistentProjectileEntity {
 	public SeaTridentEntity(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
@@ -17,12 +21,34 @@ public abstract class SeaTridentEntity extends PersistentProjectileEntity {
 	}
 
 	@Override
-	protected ItemStack asItemStack() {
-		return AquaticPlusItems.AQUA_SEA_TRIDENT.getDefaultStack();
+	protected void onEntityHit(EntityHitResult entityHitResult) {
+		Entity entity = entityHitResult.getEntity();
+		World world = entity.getWorld();
+
+		if (entity instanceof LivingEntity target) {
+			if (!world.isClient) {
+				((ServerWorld) world).spawnParticles(getParticle(), target.getX(), target.getY(), target.getZ(), 20, 0.2f, 0.1f, 0.05f, 0.5f);
+			}
+
+			target.damage(target.getDamageSources().arrow(this, this.getOwner()), 7);
+		}
+
+		super.onEntityHit(entityHitResult);
 	}
 
 	@Override
-	public boolean hasNoGravity() {
-		return true;
+	public void tick() {
+		super.tick();
+
+		if (!this.getWorld().isClient && this.age % 6 == 0) {
+			((ServerWorld) this.getWorld()).spawnParticles(ParticleTypes.ENCHANT, this.getX(), this.getY(), this.getZ(), 3, 0, 0.2f, 0.1f, 1);
+		}
+
+		if (!this.isNoClip()) {
+			Vec3d vel = this.getVelocity();
+			this.setVelocity(vel.x, vel.y - 0.001, vel.z);
+		}
 	}
+
+	abstract public ParticleEffect getParticle();
 }
